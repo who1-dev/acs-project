7a84c373d504db43cc5883abfa833a4918b50e61
Aldo Kilian Alvarado <32366750+AlvaradoA@users.noreply.github.com>
2024-12-09 12:36:00 -0500

    Added ec2-instance-connect to user data

--- dev/compute/user_data/install_httpd.sh
+++ dev/compute/user_data/install_httpd.sh
@@ -2,6 +2,7 @@
 # Install Apache Web Server
 yum -y update
 yum -y install httpd
+yum -y install ec2-instance-connect
 # Download webpage files
 curl https://raw.githubusercontent.com/AlvaradoA/acs-project-webpage/refs/heads/main/index.html -o index.html
 sudo cp index.html /var/www/html
