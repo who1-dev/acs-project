cc37edb7a288cec46e76fc1928d796e088ac57bf
Aldo Kilian Alvarado <32366750+AlvaradoA@users.noreply.github.com>
2024-12-09 12:13:03 -0500

    Edited user data for new ami

--- dev/compute/user_data/install_httpd.sh
+++ dev/compute/user_data/install_httpd.sh
@@ -2,9 +2,10 @@
 # Install Apache Web Server
 yum -y update
 yum -y install httpd
-# Download files
-wget https://github.com/AlvaradoA/acs-project-webpage/archive/refs/heads/main.zip
-unzip -p main.zip acs-project-webpage-main/index.html > /var/www/html/index.html
+# Download webpage files
+curl https://raw.githubusercontent.com/AlvaradoA/acs-project-webpage/refs/heads/main/index.html -o index.html
+sudo cp index.html /var/www/html
+rm index.html
 # Turn on web server
 sudo systemctl start httpd
 sudo systemctl enable httpd
