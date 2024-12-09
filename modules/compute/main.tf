02fb0c3640314b40c3be383c8efada547cec952e
minhtrung071190 <85272689+minhtrung071190@users.noreply.github.com>
2024-12-09 02:04:23 +0000

    change ami al2023-ami-*-x86_64

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "latest_amazon_linux" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["al2023-ami-*-x86_64"]
   }
 }
 
